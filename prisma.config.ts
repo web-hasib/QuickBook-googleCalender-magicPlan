6d1ccbbb226152b5885698791c0c030d8fc11471
engrnahiankhan <engrnahiankhan@gmail.com>
2026-02-11 14:35:06 +0600

    updated

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,12 +3,17 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
+const databaseUrl = process.env.DATABASE_URL;
+if (!databaseUrl) {
+  throw new Error("DATABASE_URL is not defined in .env");
+}
+
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DATABASE_URL"],
+    url: databaseUrl,
   },
 });
